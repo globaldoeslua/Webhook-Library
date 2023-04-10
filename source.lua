38a6ffb88e9378f6e2d3b3c4943abc54aeab50b8
global.lua <112278719+globaldoeslua@users.noreply.github.com>
2023-04-10 15:29:55 +0200

    Create source.lua

---
+++ source.lua
@@ -0,0 +1,46 @@
+local library = {}
+
+local function CreateEmbed(content, WebhookURL, title, description, link, thumbnailURL, color)
+    local url = WebhookURL
+    local data = {
+        ["content"] = content,
+        ["embeds"] = {
+            {
+                ["title"] = title,
+                ["description"] = description,
+                ["url"] = link,
+                ["thumbnail"] = {
+                    ["url"] = thumbnailURL,
+                },
+                ["type"] = "rich",
+                ["color"] = tonumber(color),
+                ["timestamp"] = DateTime.now():ToIsoDate()
+            }
+        }
+    }
+    local newdata = game:GetService("HttpService"):JSONEncode(data)
+
+    local headers = {
+        ["content-type"] = "application/json"
+    }
+    request = http_request or request or HttpPost or request or syn.request
+    local abcdef = {Url = url, Body = newdata, Method = "POST", Headers = headers}
+    request(abcdef) -- sending the request
+end
+
+local function SendMessage(content, WebhookURL)
+    local url = WebhookURL
+    local data = {
+        ["content"] = content,
+    }
+    local newdata = game:GetService("HttpService"):JSONEncode(data)
+
+    local headers = {
+        ["content-type"] = "application/json"
+    }
+    request = http_request or request or HttpPost or request or syn.request
+    local abcdef = {Url = url, Body = newdata, Method = "POST", Headers = headers}
+    request(abcdef) -- sending the request
+end
+
+return library
