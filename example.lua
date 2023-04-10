2d2f24b1f4ff44c90329b32a6275e34d95024711
global.lua <112278719+globaldoeslua@users.noreply.github.com>
2023-04-10 15:30:32 +0200

    Create example.lua

---
+++ example.lua
@@ -0,0 +1,14 @@
+SendMessage(
+    'Hello World!', -- Content
+    'WEBHOOK_URL' -- Webhook URL
+)
+
+CreateEmbed(
+    '@everyone', -- Content
+    'WEBHOOK_URL', -- Webhook URL
+    'Webhook Title', -- Webhook Title
+    'Webhook Description', -- Webhook Description
+    'Embed Link', -- makes the title a clickable link
+    'Thumbnail URL', -- Picture on the embed
+    '0x36393F' -- Embed Color in hex
+)
